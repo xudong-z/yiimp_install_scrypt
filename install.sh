d3a803d6a9b0f016321c7aae1a8b24b13de872c2
Tatar Xavatar <perrinx@outlook.fr>
2020-04-13 12:36:48 +0200

    fix admin panel

--- install.sh
+++ install.sh
@@ -387,7 +387,7 @@
     
     # Copy Files (Blocknotify,iniparser,Stratum)
     cd $HOME/yiimp
-    sudo sed -i 's/AdminRights/'AdminPanel'/' $HOME/yiimp/web/yaamp/modules/site/SiteController.php
+    sudo sed -i 's/myadmin/'AdminPanel'/' $HOME/yiimp/web/yaamp/modules/site/SiteController.php
     sudo cp -r $HOME/yiimp/web /var/
     sudo mkdir -p /var/stratum
     cd $HOME/yiimp/stratum
